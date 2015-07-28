eb11d4ae40960a420ecdd7c5ff46c63b28bb97a3
Ted Russ <teddlesruss@gmail.com>
2015-07-29 00:38:26 +1000

    Streamlined
    Loads of the example functions removed, pretty much a milestone build.

--- helloworld/HelloWorld.ino
+++ helloworld/HelloWorld.ino
@@ -57,218 +57,48 @@ void loop(void) {
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
- // Check if any reads failed and exit early (to try again).
+  // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
-  Serial.print("#S|ALLLOG|[");
-  Serial.print("Failed to read from DHT sensor!");
-  Serial.println("]#");
-  delay(6000);
+    // All this "#S|ALLLOG|[]" stuff is sent back to the PC or laptop
+    // to trigger a piece of software called "Gobetwino" which allows
+    // you to perform a few functions on the PC and send data back to
+    // the Arduino if you need to.
+    Serial.print("#S|ALLLOG|[");
+    Serial.print("Failed to read from DHT sensor!");
+    Serial.println("]#");
+    delay(6000);
   }
   else {
-  Serial.print("#S|ALLLOG|[");
-  Serial.print("  T: ");
-  Serial.print(t);
-  Serial.print(" *C \t");
-  Serial.print("H: ");
-  Serial.print(h);
-  Serial.print(" %");
-  Serial.println("]#");
-// Now put it to LCD
-  tft.fillScreen(BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextColor(WHITE);  tft.setTextSize(2);
-  tft.println("Temperature/humidity");
-  tft.println();
-  tft.setCursor(64, 32);
-  tft.setTextColor(WHITE); tft.setTextSize(2);
-  tft.println("(c) R Russ");
-  tft.setTextColor(RED);    tft.setTextSize(5);
-  tft.println();
-  tft.setTextColor(RED);    tft.setTextSize(5);
-  tft.print(" T:");
-  tft.setTextColor(YELLOW);    tft.setTextSize(5);
-  tft.println(t);
-  tft.println();
-  tft.setTextColor(RED);    tft.setTextSize(5);
-  tft.print(" H:");
-  tft.setTextColor(YELLOW);    tft.setTextSize(5);
-  tft.println(h);
-  tft.println();
-  delay(60000);
+    Serial.print("#S|ALLLOG|[");
+    Serial.print("  T: ");
+    Serial.print(t);
+    Serial.print(" *C \t");
+    Serial.print("H: ");
+    Serial.print(h);
+    Serial.print(" %");
+    Serial.println("]#");
+    // Now put it to LCD
+    tft.fillScreen(BLACK);
+    tft.setCursor(0, 0);
+    tft.setTextColor(WHITE);  tft.setTextSize(2);
+    tft.println("Temperature/humidity");
+    tft.println();
+    tft.setCursor(64, 32);
+    tft.setTextColor(WHITE); tft.setTextSize(2);
+    tft.println("(c) R Russ");
+    tft.setTextColor(RED);    tft.setTextSize(5);
+    tft.println();
+    tft.setTextColor(RED);    tft.setTextSize(5);
+    tft.print(" T:");
+    tft.setTextColor(YELLOW);    tft.setTextSize(5);
+    tft.println(t);
+    tft.println();
+    tft.setTextColor(RED);    tft.setTextSize(5);
+    tft.print(" H:");
+    tft.setTextColor(YELLOW);    tft.setTextSize(5);
+    tft.println(h);
+    tft.println();
+    delay(600000);
   }
 }
 
-//  LOADS of these functions can be scrapped as they do nothing.
-
-unsigned long testText() {
-  tft.fillScreen(BLACK);
-  tft.setCursor(0, 0);
-  tft.setTextColor(WHITE);  tft.setTextSize(2);
-  tft.println("Temperature/humidity");
-  tft.println();
-  tft.setCursor(64, 32);
-  tft.setTextColor(WHITE); tft.setTextSize(2);
-  tft.println("(c) R Russ");
-  tft.setTextColor(RED);    tft.setTextSize(5);
-  tft.println();
-  tft.println(" T: ");
-  tft.println();
-  tft.println(" H: ");
-  tft.println();
-}
-
-//   tft.fillScreen(BLACK);
-//   for(i=min(tft.width(), tft.height()); i>20; i-=6) {
-//    i2 = i / 2;
-//    tft.fillRoundRect(cx-i2, cy-i2, i, i, i/8, tft.color565(0, i, 0));
-//  }
-unsigned long testLines(uint16_t color) {
-  int           x1, y1, x2, y2,
-                w = tft.width(),
-                h = tft.height();
-
-  tft.fillScreen(BLACK);
-
-  x1 = y1 = 0;
-  y2    = h - 1;
-  for (x2 = 0; x2 < w; x2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-  x2    = w - 1;
-  for (y2 = 0; y2 < h; y2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-
-  tft.fillScreen(BLACK);
-
-  x1    = w - 1;
-  y1    = 0;
-  y2    = h - 1;
-  for (x2 = 0; x2 < w; x2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-  x2    = 0;
-  for (y2 = 0; y2 < h; y2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-
-  tft.fillScreen(BLACK);
-
-  x1    = 0;
-  y1    = h - 1;
-  y2    = 0;
-  for (x2 = 0; x2 < w; x2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-  x2    = w - 1;
-  for (y2 = 0; y2 < h; y2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-
-  tft.fillScreen(BLACK);
-
-  x1    = w - 1;
-  y1    = h - 1;
-  y2    = 0;
-  for (x2 = 0; x2 < w; x2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-  x2    = 0;
-  for (y2 = 0; y2 < h; y2 += 6) tft.drawLine(x1, y1, x2, y2, color);
-}
-
-unsigned long testFastLines(uint16_t color1, uint16_t color2) {
-  int           x, y, w = tft.width(), h = tft.height();
-
-  tft.fillScreen(BLACK);
-  for (y = 0; y < h; y += 5) tft.drawFastHLine(0, y, w, color1);
-  for (x = 0; x < w; x += 5) tft.drawFastVLine(x, 0, h, color2);
-
-}
-
-unsigned long testRects(uint16_t color) {
-  int           n, i, i2,
-                cx = tft.width()  / 2,
-                cy = tft.height() / 2;
-
-  tft.fillScreen(BLACK);
-  n     = min(tft.width(), tft.height());
-  for (i = 2; i < n; i += 6) {
-    i2 = i / 2;
-    tft.drawRect(cx - i2, cy - i2, i, i, color);
-  }
-
-}
-
-unsigned long testFilledRects(uint16_t color1, uint16_t color2) {
-  int           n, i, i2,
-                cx = tft.width()  / 2 - 1,
-                cy = tft.height() / 2 - 1;
-
-  tft.fillScreen(BLACK);
-  n = min(tft.width(), tft.height());
-  for (i = n; i > 0; i -= 6) {
-    i2    = i / 2;
-    tft.fillRect(cx - i2, cy - i2, i, i, color1);
-    // Outlines are not included in timing results
-    tft.drawRect(cx - i2, cy - i2, i, i, color2);
-  }
-}
-
-unsigned long testFilledCircles(uint8_t radius, uint16_t color) {
-  int x, y, w = tft.width(), h = tft.height(), r2 = radius * 2;
-
-  tft.fillScreen(BLACK);
-  for (x = radius; x < w; x += r2) {
-    for (y = radius; y < h; y += r2) {
-      tft.fillCircle(x, y, radius, color);
-    }
-  }
-}
-
-unsigned long testCircles(uint8_t radius, uint16_t color) {
-  int           x, y, r2 = radius * 2,
-                      w = tft.width()  + radius,
-                      h = tft.height() + radius;
-
-  // Screen is not cleared for this one -- this is
-  // intentional and does not affect the reported time.
-  for (x = 0; x < w; x += r2) {
-    for (y = 0; y < h; y += r2) {
-      tft.drawCircle(x, y, radius, color);
-    }
-  }
-}
-
-unsigned long testTriangles() {
-  int           n, i, cx = tft.width()  / 2 - 1,
-                      cy = tft.height() / 2 - 1;
-
-  tft.fillScreen(BLACK);
-  n     = min(cx, cy);
-  for (i = 0; i < n; i += 5) {
-    tft.drawTriangle(
-      cx    , cy - i, // peak
-      cx - i, cy + i, // bottom left
-      cx + i, cy + i, // bottom right
-      tft.color565(0, 0, i));
-  }
-}
-
-unsigned long testFilledTriangles() {
-  int           i, cx = tft.width()  / 2 - 1,
-                   cy = tft.height() / 2 - 1;
-
-  tft.fillScreen(BLACK);
-  for (i = min(cx, cy); i > 10; i -= 5) {
-    tft.fillTriangle(cx, cy - i, cx - i, cy + i, cx + i, cy + i,
-                     tft.color565(0, i, i));
-    tft.drawTriangle(cx, cy - i, cx - i, cy + i, cx + i, cy + i,
-                     tft.color565(i, i, 0));
-  }
-}
-
-unsigned long testRoundRects() {
-  int           w, i, i2,
-                cx = tft.width()  / 2 - 1,
-                cy = tft.height() / 2 - 1;
-
-  tft.fillScreen(BLACK);
-  w     = min(tft.width(), tft.height());
-  for (i = 0; i < w; i += 6) {
-    i2 = i / 2;
-    tft.drawRoundRect(cx - i2, cy - i2, i, i, i / 8, tft.color565(i, 0, 0));
-  }
-}
-
-unsigned long testFilledRoundRects() {
-  int           i, i2,
-                cx = tft.width()  / 2 - 1,
-                cy = tft.height() / 2 - 1;
-}
